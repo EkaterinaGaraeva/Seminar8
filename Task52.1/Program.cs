01f0df5e85194a85a649a0e141037a4d41c41389
Ваше имя <garaevaea@mail.ru>
2022-04-11 09:21:22 +0300

    Add Task52.1

---
+++ Task52.1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задается двухмерный массив, найти медиану каждого столбца
+*/
+
+int[,] Generate2DArray(int rowNumber, int colNumber, int deviation)
+{
+    int[,] arrayToReturn = new int[rowNumber, colNumber];
+    for (int i = 0; i < colNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            arrayToReturn[i, j] = new Random().Next(-deviation, deviation);
+        }
+    }
+    return arrayToReturn;
+}
+
+void PrintArray(int[,] incomingArray, string arrayName)
+{
+    Console.WriteLine($"Вывод массива {arrayName}");
+    for (int i = 0; i < incomingArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < incomingArray.GetLength(1); j++)
+        {
+            Console.Write($"{incomingArray}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("------------------------------------");
+}
+
+int[,] array = Generate2DArray(5, 5, 10);
+PrintArray(array, "Изначальный");
